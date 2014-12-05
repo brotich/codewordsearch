070610d03d887298307991e285cff2717a5de7c0
rotychbrian <rotychbrian@gmail.com>
2014-12-05 16:11:53 +0300

    added Help text and resized panel for results panel

--- CodewordSearch/form.Designer.cs
+++ CodewordSearch/form.Designer.cs
@@ -33,21 +33,24 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.Search = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.Search);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Location = new System.Drawing.Point(13, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(201, 156);
+            this.panel1.Size = new System.Drawing.Size(201, 180);
             this.panel1.TabIndex = 0;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(3, 17);
+            this.textBox1.Location = new System.Drawing.Point(3, 121);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(194, 20);
             this.textBox1.TabIndex = 0;
@@ -57,12 +60,12 @@
             this.panel2.Controls.Add(this.textBox3);
             this.panel2.Location = new System.Drawing.Point(220, 13);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(201, 156);
+            this.panel2.Size = new System.Drawing.Size(201, 180);
             this.panel2.TabIndex = 1;
             // 
             // Search
             // 
-            this.Search.Location = new System.Drawing.Point(341, 176);
+            this.Search.Location = new System.Drawing.Point(122, 147);
             this.Search.Name = "Search";
             this.Search.Size = new System.Drawing.Size(75, 23);
             this.Search.TabIndex = 2;
@@ -77,15 +80,27 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
             this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox3.Size = new System.Drawing.Size(192, 149);
+            this.textBox3.Size = new System.Drawing.Size(192, 166);
             this.textBox3.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(3, 4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(192, 111);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Enter Word from codeword puzzle to search\r\n\r\nenter Letters and numbers separated " +
+    "by \'-\'\r\ni.e\r\n\'d-3-3-d\'\r\nand press search";
             // 
             // form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(430, 202);
-            this.Controls.Add(this.Search);
+            this.ClientSize = new System.Drawing.Size(430, 203);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "form";
@@ -105,6 +120,7 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button Search;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
