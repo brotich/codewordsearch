4ade978098f45652332332e40f0468352d11471f
rotychbrian <rotychbrian@gmail.com>
2014-12-05 15:50:50 +0300

    added textbox to display the results from searching

--- CodewordSearch/form.Designer.cs
+++ CodewordSearch/form.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.Search = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -45,6 +47,13 @@
             this.panel1.Size = new System.Drawing.Size(201, 156);
             this.panel1.TabIndex = 0;
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(194, 20);
+            this.textBox2.TabIndex = 2;
+            // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(3, 17);
@@ -54,6 +63,7 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.textBox3);
             this.panel2.Location = new System.Drawing.Point(220, 13);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(201, 156);
@@ -69,12 +79,15 @@
             this.Search.UseVisualStyleBackColor = true;
             this.Search.Click += new System.EventHandler(this.Search_Click);
             // 
-            // textBox2
+            // textBox3
             // 
-            this.textBox2.Location = new System.Drawing.Point(3, 43);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(194, 20);
-            this.textBox2.TabIndex = 2;
+            this.textBox3.Location = new System.Drawing.Point(4, 4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(192, 149);
+            this.textBox3.TabIndex = 0;
             // 
             // form
             // 
@@ -88,6 +101,8 @@
             this.Text = "CodeWord Search";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -99,6 +114,7 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button Search;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
